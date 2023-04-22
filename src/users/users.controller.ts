8b642d42e049b374c1788880e2a63295470d488c
Artem <gradysnn92@yandex.ru>
2023-04-22 14:39:39 +0300

    feat: add swagger MRMMZP-4

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,11 +14,20 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LocalAuthGuard } from '../auth/local.auth.guard';
 import { AuthenticatedGuard } from '../auth/authenticated.guard';
+import { ApiBody, ApiOkResponse } from '@nestjs/swagger';
+import {
+  LoginCheckUserRequest,
+  LoginUserRequest,
+  LoginUserResponse,
+  LogoutUserResponse,
+  RegistrationUserResponse,
+} from './types';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOkResponse({ type: RegistrationUserResponse })
   @Post('/registration')
   @HttpCode(HttpStatus.CREATED)
   @Header('Content-type', 'application/json')
@@ -26,6 +35,8 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  @ApiBody({ type: LoginUserRequest })
+  @ApiOkResponse({ type: LoginUserResponse })
   @Post('/login')
   @UseGuards(LocalAuthGuard)
   @HttpCode(HttpStatus.OK)
@@ -33,12 +44,14 @@ export class UsersController {
     return { user: req.user, msg: 'Logged in' };
   }
 
+  @ApiOkResponse({ type: LoginCheckUserRequest })
   @Get('/login-check')
   @UseGuards(AuthenticatedGuard)
   loginCheck(@Request() req) {
     return req.user;
   }
 
+  @ApiOkResponse({ type: LogoutUserResponse })
   @Get('/logout')
   logout(@Request() req) {
     req.session.destroy();
